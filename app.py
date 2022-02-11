0eea8a08e0a89bbdf4563f86403c66e58b46543d
John Gabriel T J <65861382+gabriel51israel@users.noreply.github.com>
2022-02-11 18:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,url_for
 from keras.applications.resnet import ResNet50
 import cv2
 import numpy as np
